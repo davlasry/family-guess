1a75ac46224fe0a49b90e7a5241d2402e2283e97
davidlasry <lasry.david@gmail.com>
2021-02-06 17:01:12 +0200

    remove translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,12 +23,12 @@ export class AppComponent implements OnInit {
 
   constructor(public translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('he');
+    translate.setDefaultLang('en');
 
     translate.addLangs(['en', 'he']);
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('he');
+    translate.use('en');
   }
 
   ngOnInit(): void {
